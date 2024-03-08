786e4be96f33dad6e0b8e2434db40ed176d0a035
Michael Satterfield <michael.f.satterfield@gmail.com>
2024-03-08 13:10:11 -0600

    2

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar'
+
 function Chat() {
   return (
-    <div>CHat</div>
+    <Navbar />
   )
 }
 
--- client/src/pages/Upload.js
+++ client/src/pages/Upload.js
@@ -1,8 +1,50 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar'
+
 function Upload() {
+  const handleFileSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+
+    fetch('upload_file', {
+      method: 'POST',
+      body: formData
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+  }
+
   return (
-    <div>Upload</div>
+    <div>
+    <Navbar />
+     <div className="container pt-5">
+        <form encType="multipart/form-data" onSubmit={handleFileSubmit}>
+          <div className="mb-3">
+            <label htmlFor="formFile" className="form-label">
+              Upload your file
+            </label>
+            <input
+              name="file"
+              className="form-control"
+              type="file"
+              id="formFile"
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
